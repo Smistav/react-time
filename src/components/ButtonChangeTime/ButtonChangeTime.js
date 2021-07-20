841022556811357f584c79f647791890589a899c
Stas Smirnov <smistav@ya.ru>
2021-07-20 15:31:50 +0300

    add constants

--- src/components/ButtonChangeTime/ButtonChangeTime.js
+++ src/components/ButtonChangeTime/ButtonChangeTime.js
@@ -1,9 +1,10 @@
+import { BUTTON_CHG_DIRECTION } from '../utils/constants';
 function ButonChangeTime({ changeTime }) {
   function handleClick() {
     changeTime();
   }
   return (
-    <button className="App-button" onClick={handleClick}>Изменить направление</button>
+    <button className="App-button" onClick={handleClick}>{BUTTON_CHG_DIRECTION}</button>
   )
 }
 export default ButonChangeTime
--- src/components/ButtonLog/ButtonLog.js
+++ src/components/ButtonLog/ButtonLog.js
@@ -1,9 +1,11 @@
+import { BUTTON_LOG } from '../utils/constants';
+
 function ButtonLog({ time }) {
   function handleClick() {
     console.log(`${time.hours}:${time.minutes}:${time.seconds}`);
   }
   return (
-    <button className="App-button" onClick={handleClick}>Вывести в Log</button>
+    <button className="App-button" onClick={handleClick}>{BUTTON_LOG}</button>
   )
 }
 export default ButtonLog
